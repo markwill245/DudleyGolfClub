12520b30b47695e0ef4af1cffcfff3255ddfdc67
markwill245 <markwilliams55@msn.com>
2026-05-28 18:57:37 +0100

    update

---
+++ sanity.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Dudley Golf Club Admin',
+
+  // Swapped in your real Project ID here!
+  projectId: 'c54r3a5t', 
+  dataset: 'production',
+  basePath: '/studio', 
+
+  plugins: [deskTool()],
+
+  schema: {
+    types: [
+      {
+        name: 'courseConditions',
+        title: 'Course Conditions',
+        type: 'document',
+        fields: [
+          {
+            name: 'status',
+            title: 'Daily Status Notice',
+            type: 'string',
+            description: 'e.g., Course Open - No buggies on fairways today'
+          }
+        ]
+      }
+    ],
+  },
+})
