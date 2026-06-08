e85b570d1fe4db5eabaf68dcf4942e7884d8491f
mark williams <markwilliams55@msn.com>
2026-06-08 12:24:15 +0100

    Add competition photos and update league data

--- sanity.config.js
+++ sanity.config.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 
@@ -7,9 +7,9 @@ export default defineConfig({
   title: 'Dudley Golf Club Admin',
 
   // Swapped in your real Project ID here!
-  projectId: 'c54r3a5t', 
+  projectId: 'c54r3a5t',
   dataset: 'production',
-  basePath: '/studio', 
+  basePath: '/studio',
 
   plugins: [deskTool()],
 
@@ -30,37 +30,5 @@ export default defineConfig({
       }
     ],
   },
-=======
-import { defineConfig } from 'sanity'
-import { deskTool } from 'sanity/desk'
 
-export default defineConfig({
-  name: 'default',
-  title: 'Dudley Golf Club Admin',
-
-  // Swapped in your real Project ID here!
-  projectId: 'c54r3a5t', 
-  dataset: 'production',
-  basePath: '/studio', 
-
-  plugins: [deskTool()],
-
-  schema: {
-    types: [
-      {
-        name: 'courseConditions',
-        title: 'Course Conditions',
-        type: 'document',
-        fields: [
-          {
-            name: 'status',
-            title: 'Daily Status Notice',
-            type: 'string',
-            description: 'e.g., Course Open - No buggies on fairways today'
-          }
-        ]
-      }
-    ],
-  },
->>>>>>> a1349b6e417ccd8802b2cef382d95cb7bf8e6773
 })
