88abbc3ea188116e3f340ef1b1d0767b2eb412f7
mark williams <markwilliams55@msn.com>
2026-06-25 17:38:18 +0100

    Update main navigation

---
+++ update-navs.ps1
@@ -0,0 +1,18 @@
+$masterFile = "index.html"
+
+$masterContent = Get-Content $masterFile -Raw
+
+$navPattern = '(?s)<nav\s.*?</nav>'
+
+$masterNav = [regex]::Match($masterContent, $navPattern).Value
+
+Get-ChildItem -Filter *.html | ForEach-Object {
+    $file = $_.FullName
+    $content = Get-Content $file -Raw
+
+    if ($content -match $navPattern) {
+        $newContent = [regex]::Replace($content, $navPattern, $masterNav, 1)
+        Set-Content $file $newContent
+        Write-Host "Updated nav in $($_.Name)"
+    }
+}
